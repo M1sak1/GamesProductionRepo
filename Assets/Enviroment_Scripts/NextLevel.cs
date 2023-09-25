79257e43b6006863f68359a34f890326f468857d
MrWaffaffles <joshcorrigan2@gmail.com>
2023-09-25 13:25:09 +1000

    Updated menu scenes and added spikes to level 2

--- Assets/Enviroment_Scripts/NextLevel.cs
+++ Assets/Enviroment_Scripts/NextLevel.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
+	public Animator animator;
 	Collider2D player;
-	//https://bergstrand-niklas.medium.com/how-to-add-lives-and-checkpoints-in-unity-eccf68e632a9
-	private void OnTriggerEnter2D(Collider2D other) //detects if the player has hit this invisible block and changes the scene to the winScreen as the player has won 
+
+    //https://bergstrand-niklas.medium.com/how-to-add-lives-and-checkpoints-in-unity-eccf68e632a9
+    private void OnTriggerEnter2D(Collider2D other) //detects if the player has hit this invisible block and changes the scene to the winScreen as the player has won 
 	{
+		int CurrentLevel = SceneManager.GetActiveScene().buildIndex + 1;
 		player = other;
 		other.gameObject.GetComponent<Player_Movement2>().moveable = false;
 		other.gameObject.GetComponent<Player>().playWin();
-		Invoke(nameof(finishwnig), 0.6f);
+        animator.SetTrigger("FadeOut");
+        Invoke(nameof(finishwin), 0.6f);
 	}
-	private void finishwnig()
+    //Fade stuff and mading this code re-usable https://www.youtube.com/watch?v=Oadq-IrOazg&ab_channel=Brackeys
+	private void finishwin()
 	{
-		SceneManager.LoadScene("GameScene2");
+        int NextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(NextLevel);
 	}
 }
